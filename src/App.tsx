a8fb7d388d7f525cf11278e716c11615b302d572
Edward Dorta <dortaedward@gmail.com>
2023-03-06 16:48:53 -0500

    added search and changed width of container

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,26 @@ import { createClient } from "@supabase/supabase-js";
 function App() {
   const [weatherData, setWeatherData] = useState<any>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [searchTerm, setSearchTerm] = useState<string>();
+
   const supabaseUrl = "https://jwabetvamhrditzxtffd.supabase.co";
   const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
   const supabase = createClient(supabaseUrl, supabaseKey);
 
   var tomorrow = new Date();
-  tomorrow.setDate(tomorrow.getDate()+1);
-  const day = tomorrow.getDay()
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  const day = tomorrow.getDay();
   const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   useEffect(() => {
     const getData = async () => {
       setIsLoading(true);
-      const { data, error }: any = await supabase.from("weather").select().limit(1).order('id',{ascending: false});
-      console.log(typeof(data[0].weather));
+      const { data, error }: any = await supabase
+        .from("weather")
+        .select()
+        .limit(1)
+        .order("id", { ascending: false });
+      console.log(typeof data[0].weather);
       setWeatherData(data[0].weather);
       setIsLoading(false);
     };
@@ -30,17 +36,21 @@ function App() {
   return (
     <div className="bg-neutral-700 w-screen h-screen relative flex items-center justify-center overflow-hidden">
       <div
-        className={`w-[390px] h-full bg-gradient-to-bl from-sky-400 to-sky-200 rounded overflow-auto flex flex-col gap-6 px-10 py-6 ${
+        className={`min-w-[390px] h-full bg-gradient-to-bl from-sky-400 to-sky-200 rounded overflow-auto flex flex-col gap-6 px-10 py-6 ${
           isLoading ? "items-center justify-center" : ""
         }`}
       >
-        <h1 className="text-2xl text-center font-bold">La Maison Du Chocolat Shipping Weather Forcast</h1>
+        <h1 className="text-2xl text-center font-bold">
+          La Maison Du Chocolat Shipping Weather Forcast
+        </h1>
         {isLoading ? (
           <Spinner />
         ) : (
           <div className="">
+            <input type="text" onChange={(e: any) => setSearchTerm(e.target.value)} placeholder="Search..." className="my-2 p-2 rounded w-full" />
             {weatherData ? (
-              weatherData.map((el: any) => {
+              weatherData.filter((state: any) => state.state.toLowerCase().includes(searchTerm?.toLowerCase())).map((el: any) => {
+                console.log(el)
                 return (
                   <Weather
                     key={el.state}
